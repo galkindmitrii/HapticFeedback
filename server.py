c6b2da07753b9adb85ac5679fffefae451c0947b
Dmitry Galkin <galkindmitrii@gmail.com>
2013-01-06 19:30:49 +0100

    Haptics Feedback Server new version: - Logging + separate config - Server's main function better structured - Connection timeout - More docs and improved interface

--- server.py
+++ server.py
@@ -1,9 +1,36 @@
 """
 """
 import time
+import logging
+import ConfigParser
 
 import android
 import bluetooth
+
+
+try:
+    # reading bluetooth server settings from file
+    config_filename = 'serverconfig.cfg'
+    config_section_name = 'BT_server_settings'
+
+    config = ConfigParser.ConfigParser()
+    config.read(config_filename)
+
+    PORT = int(config.get(config_section_name, 'port', 0), 16)
+    SOCKET_TIMEOUT = config.getint(config_section_name, 'socket_timeout')
+    LOG_FILE = config.get(config_section_name, 'log_file')
+
+except ConfigParser.NoSectionError:
+    print "\nWarning: Failed to find section '%s' in/or configuration \
+file '%s', using default settings." % (config_section_name, config_filename)
+    PORT = 0x1001
+    SOCKET_TIMEOUT = 60
+    LOG_FILE = 'server.log'
+
+LOG = logging
+LOG.basicConfig(filename=LOG_FILE,
+                level=logging.DEBUG,
+                format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class VibratingPhone(object):
@@ -42,25 +69,75 @@ class VibratingPhone(object):
         elif data == '886313e1':
             self.pattern_four()
         else:
-            print "Non interpretable vibration code..."
+            LOG.debug('No interpretation available for code %s' % data)
+            print "\nNon interpretable vibration code..."
 
 
 class BlueToothServer(object):
     """
     A class representing bluetooth connectivity and data transfer.
     """
-    def __init__(self, port=0x1001):
+    def __init__(self):
         self.VibroPhone = VibratingPhone()
         self.server_socket = None
-        self.port = port
+
+    def show_server_info(self):
+        """
+        Shows main info concerning device.
+        """
+        Name = self.VibroPhone.Droid.bluetoothGetLocalName()
+        Address = self.VibroPhone.Droid.bluetoothGetLocalAddress()
+
+        print '\n--- Haptics Feedback Server ---'
+        print 'Name:', Name.result
+        print 'Address:', Address.result
+        print 'Port:', PORT
+        print 'Connection timeout:', SOCKET_TIMEOUT, 'sec'
+        print 'Log file:', LOG_FILE
+        print '-------------------------------'
+
+    def bind_and_receive_data(self):
+        """
+        Binds to server socket, accepts connection and
+        returns received first 1024 bytes of data.
+
+        Data will be received only from connection that
+        was activated first, other data is ignored.
+
+        Active connection is closed after
+        SOCKET_TIMEOUT if no data was received.
+        """
+        self.server_socket.bind(("", PORT))
+
+        # This setting ensures that data from one connection will be received:
+        self.server_socket.listen(1)
+
+        client_socket, address = self.server_socket.accept()
+        print "\nAccepted connection from a device with address: ", address[0]
+
+        client_socket.settimeout(SOCKET_TIMEOUT)
+
+        data = None
+        try:
+            data = client_socket.recv(1024)
+            print "\nFinished receiving data."
+        except bluetooth.btcommon.BluetoothError as exc:
+            # if error occures - ignoring and closing sockets
+            LOG.debug('Exception during data transfer: %s' % exc)
+            print '\nConnection timeout. Closing...'
+
+        client_socket.close()
+        self.server_socket.close()
+        print "\nClosed connection from a device with address: ", address[0]
+        return data
 
     def process_data(self, data):
         """
-        A method to decide whether data is a correct
-        vibration code or something other.
+        A method to decide whether received data is a
+        correct vibration code or something else.
         """
         if data.startswith('hvc0#_'):
-            print "Received a vibration code: %s" % data
+            LOG.info("Received a vibration code: %s" % data)
             self.VibroPhone.vibrate(data[6:])
         else:
             print "Received data: %s is not a vibration code. Ignoring." % data
@@ -69,22 +146,16 @@ class BlueToothServer(object):
         """
         A method to wait for incoming BT connections and receive data.
         """
+        LOG.info('Haptics Feedback Server application started.')
+        self.show_server_info()
+
         while True:
             self.server_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
 
-            print "Waiting for incoming BT L2CAP connections..."
-            self.server_socket.bind(("", self.port))
-            self.server_socket.listen(1)
-            client_socket, address = self.server_socket.accept()
-            print "Accepted connection from a device with address: ", address
-
-            data = client_socket.recv(1024)
-            print "Finished receiving data."
-
-            client_socket.close()
-            self.server_socket.close()
-            print "Closed connection from a device with address: ", address
-            self.process_data(data)
+            print "\nWaiting for incoming BT L2CAP connections..."
+            data = self.bind_and_receive_data()
+            if data:
+                self.process_data(data)
 
 
 if __name__ == '__main__':
